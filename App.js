521d6eeb7b398962771b2d503c4ef5fd2a089bab
Eren Kahraman <kahraman1234eren@gmail.com>
2021-10-03 20:52:51 +0300

    Final version of the app

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  Button,
-  Vibration,
-  Alert,
-  Dimensions,
-} from "react-native";
+import { Text, View, Image, Dimensions } from "react-native";
 
 export default function App() {
   console.log("App Executed");
@@ -19,14 +9,113 @@ export default function App() {
     <View
       style={{
         flex: 1,
-        backgroundColor: "#343e7a",
+        backgroundColor: "#202020",
         display: "flex",
         justifyContent: "flex-end",
       }}
     >
+      <View
+        style={{
+          zIndex: 3,
+          position: "absolute",
+          backgroundColor: "#252525",
+          opacity: 0.8,
+          borderRadius: "10%",
+          width: "45%",
+          height: "5%",
+          left: "50%",
+          bottom: "85%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 18,
+          }}
+        >
+          Mediterranean Sea
+        </Text>
+      </View>
+
+      <View
+        style={{
+          zIndex: 3,
+          position: "absolute",
+          backgroundColor: "#202020",
+          opacity: 0.8,
+          borderRadius: "10%",
+          width: "40%",
+          height: "8%",
+          left: "60%",
+          bottom: "45%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            zIndex: 4,
+            fontSize: 11,
+            color: "white",
+          }}
+        >
+          Depth: 2700m {"\n"}Density: Medium {"\n"}Diameter: 100m {"\n"}Average
+          Size of Plastics: 5cm
+        </Text>
+      </View>
+
+      <Image
+        source={require("./assets/google-map-pin-icon-png-0.jpeg")}
+        style={{
+          height: "5%",
+          width: "8%",
+          position: "absolute",
+          zIndex: 2,
+          bottom: "45%",
+          left: "5%",
+        }}
+      ></Image>
+      <Image
+        source={require("./assets/google-map-pin-icon-png-0.jpeg")}
+        style={{
+          height: "5%",
+          width: "8%",
+          position: "absolute",
+          zIndex: 2,
+          bottom: "32%",
+          left: "80%",
+        }}
+      ></Image>
+      <Image
+        source={require("./assets/google-map-pin-icon-png-0.jpeg")}
+        style={{
+          height: "9.5%",
+          width: "15%",
+          position: "absolute",
+          zIndex: 2,
+          bottom: "37%",
+          left: "55%",
+        }}
+      ></Image>
+      <Image
+        source={require("./assets/google-map-pin-icon-png-0.jpeg")}
+        style={{
+          height: "5%",
+          width: "8%",
+          position: "absolute",
+          zIndex: 2,
+          bottom: "35%",
+          left: "25%",
+        }}
+      ></Image>
+
       <Image
         source={{
-          uri: "https://www.google.com/maps/d/thumbnail?mid=1rcj1mUZmtMhyCMLEDuTwvlLDTHo",
+          uri: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Mediterranean_Basin_and_Near_East_before_1000_AD_topographic_map.jpg",
           width: "100%",
           height: "82%",
         }}
@@ -49,20 +138,56 @@ export default function App() {
           style={{
             backgroundColor: "#202020",
             flex: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
           }}
-        />
+        >
+          <Image
+            source={require("./assets/Information-icon.png")}
+            style={{
+              height: "50%",
+              width: "50%",
+            }}
+          ></Image>
+        </View>
         <View
           style={{
             backgroundColor: "#252525",
             flex: 2,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
           }}
-        />
+        >
+          <Image
+            source={require("./assets/2820184-200.png")}
+            style={{
+              height: "80%",
+              width: "40%",
+            }}
+          ></Image>
+        </View>
         <View
           style={{
             backgroundColor: "#202020",
             flex: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
           }}
-        />
+        >
+          <Image
+            source={require("./assets/126472.png")}
+            style={{
+              height: "50%",
+              width: "50%",
+            }}
+          ></Image>
+        </View>
       </View>
     </View>
   );
